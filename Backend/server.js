6db2c96376159c4d77df25ec749363851782c815
Islam-Tarek <islamtarek258@gmail.com>
2025-05-02 12:26:24 +0300

    feat: add backend for the project

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+// server.js
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+app.db = router.db;
+
+app.use(cors());
+app.use(jsonServer.bodyParser);
+app.use(auth);
+app.use(router);
+
+app.listen(3000, () => {
+  console.log("JSON Server with Auth is running on http://localhost:3000");
+});
